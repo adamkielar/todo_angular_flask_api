f5908f5b23f0620f01eba8945225f581cd81ac94
Adam Kielar <adam@adamkielar.pl>
2020-04-01 17:08:13 +0200

    add put and delete for Todo

--- test_todo_app.py
+++ test_todo_app.py
@@ -28,16 +28,16 @@ class TodoModelTestCase(unittest.TestCase):
         test_db.connect()
         test_db.create_tables([Todo], safe=True)
 
-    def tearDown(self):
-        test_db.drop_tables([Todo])
-        test_db.close()
-
     def test_todo_creation(self):
         Todo.create(
             name='run today',
         )
 
         self.assertEqual(Todo.select().count(), 1)
+
+    def tearDown(self):
+        test_db.drop_tables([Todo])
+        test_db.close()
 
 
 class ViewTestCase(unittest.TestCase):
@@ -71,7 +71,7 @@ class ApiTestCase(ViewTestCase):
     def test_get_todo_list(self):
         response = self.app.get("/api/v1/todos")
         self.assertEqual(response.status_code, 200)
-        
+
     def test_post_todo_list(self):
         todo = json.dumps({
             "name": "run 5km",
@@ -80,6 +80,34 @@ class ApiTestCase(ViewTestCase):
         })
         response = self.app.post("/api/v1/todos", data=todo, headers={"Content-Type": "application/json"})
         self.assertEqual(response.status_code, 201)
+        self.assertIn("run 5km", response.get_data(as_text=True))
+
+    def test_get_todo_item(self):
+        response = self.app.get("/api/v1/todos/1")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("clean the house", response.get_data(as_text=True))
+
+    def test_put_todo_item(self):
+        todo = json.dumps({
+            "id": 6,
+            "name": "swim now",
+            "completed": "false",
+            "edited": "true"
+        })
+        response = self.app.put("/api/v1/todos/6", data=todo, headers={"Content-Type": "application/json"})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("swim now", response.get_data(as_text=True))
+
+    def test_delete_todo_item(self):
+        todo = json.dumps({
+            "id": 6,
+            "name": "swim now",
+            "completed": "false",
+            "edited": "true"
+        })
+        response = self.app.delete("/api/v1/todos/6", data=todo, headers={"Content-Type": "application/json"})
+        self.assertEqual(response.status_code, 204)
+        self.assertNotIn("swim now", response.get_data(as_text=True))
 
 
 if __name__ == '__main__':
